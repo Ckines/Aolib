58a7043007a33253861feb8f50b2d7626e75803a
J. Castro <jvgc1991@gmail.com>
2026-08-24 17:09:36 +0200

    Add files via upload

---
+++ deps/vgmstream/meta/ahx.d
@@ -0,0 +1,48 @@
+deps/vgmstream/meta/ahx.o: deps/vgmstream/meta/ahx.c \
+ deps/vgmstream/meta/meta.h deps/vgmstream/meta/../vgmstream.h \
+ deps/vgmstream/meta/../util/vgmstream_limits.h \
+ deps/vgmstream/meta/../streamfile.h deps/vgmstream/meta/../streamtypes.h \
+ deps/vgmstream/meta/../util.h deps/vgmstream/meta/../util/reader_get.h \
+ deps/vgmstream/meta/../util/reader_put.h \
+ deps/vgmstream/meta/../vgmstream_types.h \
+ deps/vgmstream/meta/../coding/libs/g72x_vgmstream.h \
+ deps/vgmstream/meta/../util/reader_sf.h \
+ deps/vgmstream/meta/../util/../streamfile.h \
+ deps/vgmstream/meta/../util/reader_get.h \
+ deps/vgmstream/meta/../util/reader_text.h \
+ deps/vgmstream/meta/../util/sf_utils.h deps/vgmstream/meta/../util/log.h \
+ deps/vgmstream/meta/../coding/coding.h \
+ deps/vgmstream/meta/../coding/../vgmstream.h \
+ deps/vgmstream/meta/../coding/../util/reader_sf.h \
+ deps/vgmstream/meta/../coding/../util/reader_get_nibbles.h \
+ deps/vgmstream/meta/../coding/../util/../streamtypes.h \
+ deps/vgmstream/meta/../coding/../util/log.h \
+ deps/vgmstream/meta/../coding/libs/clhca.h \
+ deps/vgmstream/meta/ahx_keys.h deps/vgmstream/meta/../util/cri_keys.h \
+ deps/vgmstream/meta/../util/companion_files.h
+deps/vgmstream/meta/meta.h:
+deps/vgmstream/meta/../vgmstream.h:
+deps/vgmstream/meta/../util/vgmstream_limits.h:
+deps/vgmstream/meta/../streamfile.h:
+deps/vgmstream/meta/../streamtypes.h:
+deps/vgmstream/meta/../util.h:
+deps/vgmstream/meta/../util/reader_get.h:
+deps/vgmstream/meta/../util/reader_put.h:
+deps/vgmstream/meta/../vgmstream_types.h:
+deps/vgmstream/meta/../coding/libs/g72x_vgmstream.h:
+deps/vgmstream/meta/../util/reader_sf.h:
+deps/vgmstream/meta/../util/../streamfile.h:
+deps/vgmstream/meta/../util/reader_get.h:
+deps/vgmstream/meta/../util/reader_text.h:
+deps/vgmstream/meta/../util/sf_utils.h:
+deps/vgmstream/meta/../util/log.h:
+deps/vgmstream/meta/../coding/coding.h:
+deps/vgmstream/meta/../coding/../vgmstream.h:
+deps/vgmstream/meta/../coding/../util/reader_sf.h:
+deps/vgmstream/meta/../coding/../util/reader_get_nibbles.h:
+deps/vgmstream/meta/../coding/../util/../streamtypes.h:
+deps/vgmstream/meta/../coding/../util/log.h:
+deps/vgmstream/meta/../coding/libs/clhca.h:
+deps/vgmstream/meta/ahx_keys.h:
+deps/vgmstream/meta/../util/cri_keys.h:
+deps/vgmstream/meta/../util/companion_files.h:
---
+++ deps/vgmstream/meta/deblock_streamfile.d
@@ -0,0 +1,14 @@
+deps/vgmstream/meta/deblock_streamfile.o: \
+ deps/vgmstream/meta/deblock_streamfile.c \
+ deps/vgmstream/meta/deblock_streamfile.h \
+ deps/vgmstream/meta/../streamfile.h deps/vgmstream/meta/../streamtypes.h \
+ deps/vgmstream/meta/../util.h deps/vgmstream/meta/../util/reader_get.h \
+ deps/vgmstream/meta/../util/reader_put.h \
+ deps/vgmstream/meta/../util/log.h
+deps/vgmstream/meta/deblock_streamfile.h:
+deps/vgmstream/meta/../streamfile.h:
+deps/vgmstream/meta/../streamtypes.h:
+deps/vgmstream/meta/../util.h:
+deps/vgmstream/meta/../util/reader_get.h:
+deps/vgmstream/meta/../util/reader_put.h:
+deps/vgmstream/meta/../util/log.h:
