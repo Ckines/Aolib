82daf3cf71aa87cd6f3f02037dbf1979f7eabf8c
J. Castro <jvgc1991@gmail.com>
2026-08-24 17:33:18 +0200

    Add files via upload

---
+++ src/libretro.d
@@ -0,0 +1,120 @@
+src/libretro.o: src/libretro.cpp src/libretro.h src/core_context.hpp \
+ src/engine/iaudio_engine.hpp src/engine/../vfs_bridge.hpp \
+ src/engine/../libretro.h src/zip_playlist.hpp \
+ src/vgmstream_extensions.hpp src/zip_vfs_adapter.hpp \
+ deps/aosdk/zlib/zconf.h deps/aosdk/zlib/zlib.h deps/aosdk/zlib/zconf.h \
+ deps/minizip/ioapi.h deps/minizip/unzip.h deps/minizip/ioapi.h \
+ src/dsp/reverb.hpp src/sevenzip_playlist.hpp \
+ src/sevenzip_vfs_adapter.hpp deps/sevenzip/7zTypes.h deps/sevenzip/7z.h \
+ deps/sevenzip/7zTypes.h deps/sevenzip/7zAlloc.h deps/sevenzip/7zCrc.h \
+ src/engine/gme_engine.hpp deps/game-music-emu/gme/gme.h \
+ src/engine/libvgm_engine.hpp deps/libvgm/utils/DataLoader.h \
+ deps/libvgm/utils/../stdtype.h deps/libvgm/utils/MemoryLoader.h \
+ deps/libvgm/utils/DataLoader.h deps/libvgm/emu/EmuStructs.h \
+ deps/libvgm/emu/../stdtype.h deps/libvgm/emu/snddef.h \
+ deps/libvgm/emu/SoundEmu.h deps/libvgm/emu/EmuStructs.h \
+ deps/libvgm/player/playera.hpp deps/libvgm/player/../stdtype.h \
+ deps/libvgm/player/../utils/DataLoader.h \
+ deps/libvgm/player/../emu/Resampler.h \
+ deps/libvgm/player/../emu/../stdtype.h \
+ deps/libvgm/player/../emu/snddef.h \
+ deps/libvgm/player/../emu/EmuStructs.h deps/libvgm/player/playerbase.hpp \
+ deps/libvgm/player/../emu/EmuStructs.h deps/libvgm/player/playerbase.hpp \
+ deps/libvgm/player/vgmplayer.hpp deps/libvgm/player/../utils/StrUtils.h \
+ deps/libvgm/player/../utils/../stdtype.h deps/libvgm/player/helper.h \
+ deps/libvgm/player/../emu/logging.h \
+ deps/libvgm/player/../emu/../common_def.h \
+ deps/libvgm/player/../emu/../stdtype.h \
+ deps/libvgm/player/../emu/../_stdbool.h deps/libvgm/player/dblk_compr.h \
+ deps/libvgm/player/../common_def.h src/engine/xmp_engine.hpp \
+ deps/libxmp-lite/include/xmp.h src/engine/vgmstream_engine.hpp \
+ src/engine/vgmstream_api.hpp src/engine/../vgmstream_vfs_adapter.hpp \
+ deps/vgmstream/libvgmstream.h deps/vgmstream/libvgmstream_streamfile.h \
+ deps/vgmstream/libvgmstream.h deps/vgmstream/libvgmstream_streamfile.h \
+ src/ui/audio_analyzer.hpp src/ui/transport.hpp src/ui/ui_model.hpp \
+ src/ui/ui_screen.hpp src/ui/ui_font.hpp src/ui/ui_renderer.hpp \
+ src/engine/psf_engine.hpp src/engine/aosdk_bridge.hpp deps/aosdk/ao.h \
+ deps/aosdk/win32_utf8/src/entry.h deps/aosdk/wavedump.h \
+ deps/aosdk/sampledump.h deps/aosdk/corlett.h deps/aosdk/utils.h \
+ deps/aosdk/eng_protos.h src/engine/psf2_engine.hpp \
+ src/engine/ssf_engine.hpp
+src/libretro.h:
+src/core_context.hpp:
+src/engine/iaudio_engine.hpp:
+src/engine/../vfs_bridge.hpp:
+src/engine/../libretro.h:
+src/zip_playlist.hpp:
+src/vgmstream_extensions.hpp:
+src/zip_vfs_adapter.hpp:
+deps/aosdk/zlib/zconf.h:
+deps/aosdk/zlib/zlib.h:
+deps/aosdk/zlib/zconf.h:
+deps/minizip/ioapi.h:
+deps/minizip/unzip.h:
+deps/minizip/ioapi.h:
+src/dsp/reverb.hpp:
+src/sevenzip_playlist.hpp:
+src/sevenzip_vfs_adapter.hpp:
+deps/sevenzip/7zTypes.h:
+deps/sevenzip/7z.h:
+deps/sevenzip/7zTypes.h:
+deps/sevenzip/7zAlloc.h:
+deps/sevenzip/7zCrc.h:
+src/engine/gme_engine.hpp:
+deps/game-music-emu/gme/gme.h:
+src/engine/libvgm_engine.hpp:
+deps/libvgm/utils/DataLoader.h:
+deps/libvgm/utils/../stdtype.h:
+deps/libvgm/utils/MemoryLoader.h:
+deps/libvgm/utils/DataLoader.h:
+deps/libvgm/emu/EmuStructs.h:
+deps/libvgm/emu/../stdtype.h:
+deps/libvgm/emu/snddef.h:
+deps/libvgm/emu/SoundEmu.h:
+deps/libvgm/emu/EmuStructs.h:
+deps/libvgm/player/playera.hpp:
+deps/libvgm/player/../stdtype.h:
+deps/libvgm/player/../utils/DataLoader.h:
+deps/libvgm/player/../emu/Resampler.h:
+deps/libvgm/player/../emu/../stdtype.h:
+deps/libvgm/player/../emu/snddef.h:
+deps/libvgm/player/../emu/EmuStructs.h:
+deps/libvgm/player/playerbase.hpp:
+deps/libvgm/player/../emu/EmuStructs.h:
+deps/libvgm/player/playerbase.hpp:
+deps/libvgm/player/vgmplayer.hpp:
+deps/libvgm/player/../utils/StrUtils.h:
+deps/libvgm/player/../utils/../stdtype.h:
+deps/libvgm/player/helper.h:
+deps/libvgm/player/../emu/logging.h:
+deps/libvgm/player/../emu/../common_def.h:
+deps/libvgm/player/../emu/../stdtype.h:
+deps/libvgm/player/../emu/../_stdbool.h:
+deps/libvgm/player/dblk_compr.h:
+deps/libvgm/player/../common_def.h:
+src/engine/xmp_engine.hpp:
+deps/libxmp-lite/include/xmp.h:
+src/engine/vgmstream_engine.hpp:
+src/engine/vgmstream_api.hpp:
+src/engine/../vgmstream_vfs_adapter.hpp:
+deps/vgmstream/libvgmstream.h:
+deps/vgmstream/libvgmstream_streamfile.h:
+deps/vgmstream/libvgmstream.h:
+deps/vgmstream/libvgmstream_streamfile.h:
+src/ui/audio_analyzer.hpp:
+src/ui/transport.hpp:
+src/ui/ui_model.hpp:
+src/ui/ui_screen.hpp:
+src/ui/ui_font.hpp:
+src/ui/ui_renderer.hpp:
+src/engine/psf_engine.hpp:
+src/engine/aosdk_bridge.hpp:
+deps/aosdk/ao.h:
+deps/aosdk/win32_utf8/src/entry.h:
+deps/aosdk/wavedump.h:
+deps/aosdk/sampledump.h:
+deps/aosdk/corlett.h:
+deps/aosdk/utils.h:
+deps/aosdk/eng_protos.h:
+src/engine/psf2_engine.hpp:
+src/engine/ssf_engine.hpp:
