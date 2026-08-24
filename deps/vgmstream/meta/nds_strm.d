c57795b2784de8958bf6c0ceaca8ee409b29798d
J. Castro <jvgc1991@gmail.com>
2026-08-24 17:11:16 +0200

    Add files via upload

---
+++ deps/vgmstream/meta/nds_strm.d
@@ -0,0 +1,31 @@
+deps/vgmstream/meta/nds_strm.o: deps/vgmstream/meta/nds_strm.c \
+ deps/vgmstream/meta/meta.h deps/vgmstream/meta/../vgmstream.h \
+ deps/vgmstream/meta/../util/vgmstream_limits.h \
+ deps/vgmstream/meta/../streamfile.h deps/vgmstream/meta/../streamtypes.h \
+ deps/vgmstream/meta/../util.h deps/vgmstream/meta/../util/reader_get.h \
+ deps/vgmstream/meta/../util/reader_put.h \
+ deps/vgmstream/meta/../vgmstream_types.h \
+ deps/vgmstream/meta/../coding/libs/g72x_vgmstream.h \
+ deps/vgmstream/meta/../util/reader_sf.h \
+ deps/vgmstream/meta/../util/../streamfile.h \
+ deps/vgmstream/meta/../util/reader_get.h \
+ deps/vgmstream/meta/../util/reader_text.h \
+ deps/vgmstream/meta/../util/sf_utils.h deps/vgmstream/meta/../util/log.h \
+ deps/vgmstream/meta/../util.h
+deps/vgmstream/meta/meta.h:
+deps/vgmstream/meta/../vgmstream.h:
+deps/vgmstream/meta/../util/vgmstream_limits.h:
+deps/vgmstream/meta/../streamfile.h:
+deps/vgmstream/meta/../streamtypes.h:
+deps/vgmstream/meta/../util.h:
+deps/vgmstream/meta/../util/reader_get.h:
+deps/vgmstream/meta/../util/reader_put.h:
+deps/vgmstream/meta/../vgmstream_types.h:
+deps/vgmstream/meta/../coding/libs/g72x_vgmstream.h:
+deps/vgmstream/meta/../util/reader_sf.h:
+deps/vgmstream/meta/../util/../streamfile.h:
+deps/vgmstream/meta/../util/reader_get.h:
+deps/vgmstream/meta/../util/reader_text.h:
+deps/vgmstream/meta/../util/sf_utils.h:
+deps/vgmstream/meta/../util/log.h:
+deps/vgmstream/meta/../util.h:
