913512b7226885f7d1a8ff29625e6e2ad9beeb62
J. Castro <jvgc1991@gmail.com>
2026-08-24 16:55:23 +0200

    Add files via upload

---
+++ deps/vgmstream/coding/ubi_mpeg_decoder.d
@@ -0,0 +1,53 @@
+deps/vgmstream/coding/ubi_mpeg_decoder.o: \
+ deps/vgmstream/coding/ubi_mpeg_decoder.c deps/vgmstream/coding/coding.h \
+ deps/vgmstream/coding/../vgmstream.h \
+ deps/vgmstream/coding/../util/vgmstream_limits.h \
+ deps/vgmstream/coding/../streamfile.h \
+ deps/vgmstream/coding/../streamtypes.h deps/vgmstream/coding/../util.h \
+ deps/vgmstream/coding/../util/reader_get.h \
+ deps/vgmstream/coding/../util/reader_put.h \
+ deps/vgmstream/coding/../vgmstream_types.h \
+ deps/vgmstream/coding/../coding/libs/g72x_vgmstream.h \
+ deps/vgmstream/coding/../util/reader_sf.h \
+ deps/vgmstream/coding/../util/../streamfile.h \
+ deps/vgmstream/coding/../util/reader_get.h \
+ deps/vgmstream/coding/../util/reader_get_nibbles.h \
+ deps/vgmstream/coding/../util/../streamtypes.h \
+ deps/vgmstream/coding/../util/log.h deps/vgmstream/coding/libs/clhca.h \
+ deps/vgmstream/coding/../base/decode_state.h \
+ deps/vgmstream/coding/../base/sbuf.h \
+ deps/vgmstream/coding/../base/../streamtypes.h \
+ deps/vgmstream/coding/../base/codec_info.h \
+ deps/vgmstream/coding/../base/../vgmstream.h \
+ deps/vgmstream/coding/../base/../base/sbuf.h \
+ deps/vgmstream/coding/libs/ubi_mpeg_helpers.h \
+ deps/vgmstream/coding/libs/../../util/bitstream_msb.h \
+ deps/vgmstream/coding/libs/minimp3_vgmstream.h \
+ deps/vgmstream/coding/libs/minimp3.h
+deps/vgmstream/coding/coding.h:
+deps/vgmstream/coding/../vgmstream.h:
+deps/vgmstream/coding/../util/vgmstream_limits.h:
+deps/vgmstream/coding/../streamfile.h:
+deps/vgmstream/coding/../streamtypes.h:
+deps/vgmstream/coding/../util.h:
+deps/vgmstream/coding/../util/reader_get.h:
+deps/vgmstream/coding/../util/reader_put.h:
+deps/vgmstream/coding/../vgmstream_types.h:
+deps/vgmstream/coding/../coding/libs/g72x_vgmstream.h:
+deps/vgmstream/coding/../util/reader_sf.h:
+deps/vgmstream/coding/../util/../streamfile.h:
+deps/vgmstream/coding/../util/reader_get.h:
+deps/vgmstream/coding/../util/reader_get_nibbles.h:
+deps/vgmstream/coding/../util/../streamtypes.h:
+deps/vgmstream/coding/../util/log.h:
+deps/vgmstream/coding/libs/clhca.h:
+deps/vgmstream/coding/../base/decode_state.h:
+deps/vgmstream/coding/../base/sbuf.h:
+deps/vgmstream/coding/../base/../streamtypes.h:
+deps/vgmstream/coding/../base/codec_info.h:
+deps/vgmstream/coding/../base/../vgmstream.h:
+deps/vgmstream/coding/../base/../base/sbuf.h:
+deps/vgmstream/coding/libs/ubi_mpeg_helpers.h:
+deps/vgmstream/coding/libs/../../util/bitstream_msb.h:
+deps/vgmstream/coding/libs/minimp3_vgmstream.h:
+deps/vgmstream/coding/libs/minimp3.h:
---
+++ deps/vgmstream/coding/vorbis_custom_decoder.d
@@ -0,0 +1,54 @@
+deps/vgmstream/coding/vorbis_custom_decoder.o: \
+ deps/vgmstream/coding/vorbis_custom_decoder.c \
+ deps/vgmstream/coding/../base/decode_state.h \
+ deps/vgmstream/coding/../base/sbuf.h \
+ deps/vgmstream/coding/../base/../streamtypes.h \
+ deps/vgmstream/coding/../base/sbuf.h \
+ deps/vgmstream/coding/../base/codec_info.h \
+ deps/vgmstream/coding/../base/../vgmstream.h \
+ deps/vgmstream/coding/../base/../util/vgmstream_limits.h \
+ deps/vgmstream/coding/../base/../streamfile.h \
+ deps/vgmstream/coding/../base/../streamtypes.h \
+ deps/vgmstream/coding/../base/../util.h \
+ deps/vgmstream/coding/../base/../util/reader_get.h \
+ deps/vgmstream/coding/../base/../util/reader_put.h \
+ deps/vgmstream/coding/../base/../vgmstream_types.h \
+ deps/vgmstream/coding/../base/../coding/libs/g72x_vgmstream.h \
+ deps/vgmstream/coding/../base/../base/sbuf.h \
+ deps/vgmstream/coding/../base/seek_table.h \
+ deps/vgmstream/coding/coding.h deps/vgmstream/coding/../vgmstream.h \
+ deps/vgmstream/coding/../util/reader_sf.h \
+ deps/vgmstream/coding/../util/../streamfile.h \
+ deps/vgmstream/coding/../util/reader_get.h \
+ deps/vgmstream/coding/../util/reader_get_nibbles.h \
+ deps/vgmstream/coding/../util/../streamtypes.h \
+ deps/vgmstream/coding/../util/log.h deps/vgmstream/coding/libs/clhca.h \
+ deps/vgmstream/coding/vorbis_custom_decoder.h \
+ deps/vgmstream/coding/../coding/coding.h
+deps/vgmstream/coding/../base/decode_state.h:
+deps/vgmstream/coding/../base/sbuf.h:
+deps/vgmstream/coding/../base/../streamtypes.h:
+deps/vgmstream/coding/../base/sbuf.h:
+deps/vgmstream/coding/../base/codec_info.h:
+deps/vgmstream/coding/../base/../vgmstream.h:
+deps/vgmstream/coding/../base/../util/vgmstream_limits.h:
+deps/vgmstream/coding/../base/../streamfile.h:
+deps/vgmstream/coding/../base/../streamtypes.h:
+deps/vgmstream/coding/../base/../util.h:
+deps/vgmstream/coding/../base/../util/reader_get.h:
+deps/vgmstream/coding/../base/../util/reader_put.h:
+deps/vgmstream/coding/../base/../vgmstream_types.h:
+deps/vgmstream/coding/../base/../coding/libs/g72x_vgmstream.h:
+deps/vgmstream/coding/../base/../base/sbuf.h:
+deps/vgmstream/coding/../base/seek_table.h:
+deps/vgmstream/coding/coding.h:
+deps/vgmstream/coding/../vgmstream.h:
+deps/vgmstream/coding/../util/reader_sf.h:
+deps/vgmstream/coding/../util/../streamfile.h:
+deps/vgmstream/coding/../util/reader_get.h:
+deps/vgmstream/coding/../util/reader_get_nibbles.h:
+deps/vgmstream/coding/../util/../streamtypes.h:
+deps/vgmstream/coding/../util/log.h:
+deps/vgmstream/coding/libs/clhca.h:
+deps/vgmstream/coding/vorbis_custom_decoder.h:
+deps/vgmstream/coding/../coding/coding.h:
