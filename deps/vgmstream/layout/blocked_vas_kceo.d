462c5c464926d28d595995492216f858f3295809
J. Castro <jvgc1991@gmail.com>
2026-08-24 17:04:16 +0200

    Add files via upload

---
+++ deps/vgmstream/layout/blocked_vas_kceo.d
@@ -0,0 +1,35 @@
+deps/vgmstream/layout/blocked_vas_kceo.o: \
+ deps/vgmstream/layout/blocked_vas_kceo.c deps/vgmstream/layout/layout.h \
+ deps/vgmstream/layout/../streamtypes.h \
+ deps/vgmstream/layout/../vgmstream.h \
+ deps/vgmstream/layout/../util/vgmstream_limits.h \
+ deps/vgmstream/layout/../streamfile.h \
+ deps/vgmstream/layout/../streamtypes.h deps/vgmstream/layout/../util.h \
+ deps/vgmstream/layout/../util/reader_get.h \
+ deps/vgmstream/layout/../util/reader_put.h \
+ deps/vgmstream/layout/../vgmstream_types.h \
+ deps/vgmstream/layout/../coding/libs/g72x_vgmstream.h \
+ deps/vgmstream/layout/../util/reader_sf.h \
+ deps/vgmstream/layout/../util/../streamfile.h \
+ deps/vgmstream/layout/../util/reader_get.h \
+ deps/vgmstream/layout/../util/log.h deps/vgmstream/layout/../base/sbuf.h \
+ deps/vgmstream/layout/../base/../streamtypes.h \
+ deps/vgmstream/layout/../base/rc.h
+deps/vgmstream/layout/layout.h:
+deps/vgmstream/layout/../streamtypes.h:
+deps/vgmstream/layout/../vgmstream.h:
+deps/vgmstream/layout/../util/vgmstream_limits.h:
+deps/vgmstream/layout/../streamfile.h:
+deps/vgmstream/layout/../streamtypes.h:
+deps/vgmstream/layout/../util.h:
+deps/vgmstream/layout/../util/reader_get.h:
+deps/vgmstream/layout/../util/reader_put.h:
+deps/vgmstream/layout/../vgmstream_types.h:
+deps/vgmstream/layout/../coding/libs/g72x_vgmstream.h:
+deps/vgmstream/layout/../util/reader_sf.h:
+deps/vgmstream/layout/../util/../streamfile.h:
+deps/vgmstream/layout/../util/reader_get.h:
+deps/vgmstream/layout/../util/log.h:
+deps/vgmstream/layout/../base/sbuf.h:
+deps/vgmstream/layout/../base/../streamtypes.h:
+deps/vgmstream/layout/../base/rc.h:
---
+++ deps/vgmstream/layout/segmented.d
@@ -0,0 +1,51 @@
+deps/vgmstream/layout/segmented.o: deps/vgmstream/layout/segmented.c \
+ deps/vgmstream/layout/layout.h deps/vgmstream/layout/../streamtypes.h \
+ deps/vgmstream/layout/../vgmstream.h \
+ deps/vgmstream/layout/../util/vgmstream_limits.h \
+ deps/vgmstream/layout/../streamfile.h \
+ deps/vgmstream/layout/../streamtypes.h deps/vgmstream/layout/../util.h \
+ deps/vgmstream/layout/../util/reader_get.h \
+ deps/vgmstream/layout/../util/reader_put.h \
+ deps/vgmstream/layout/../vgmstream_types.h \
+ deps/vgmstream/layout/../coding/libs/g72x_vgmstream.h \
+ deps/vgmstream/layout/../util/reader_sf.h \
+ deps/vgmstream/layout/../util/../streamfile.h \
+ deps/vgmstream/layout/../util/reader_get.h \
+ deps/vgmstream/layout/../util/log.h deps/vgmstream/layout/../base/sbuf.h \
+ deps/vgmstream/layout/../base/../streamtypes.h \
+ deps/vgmstream/layout/../base/rc.h deps/vgmstream/layout/../base/seek.h \
+ deps/vgmstream/layout/../base/../vgmstream.h \
+ deps/vgmstream/layout/../base/decode.h \
+ deps/vgmstream/layout/../base/sbuf.h \
+ deps/vgmstream/layout/../base/mixing.h \
+ deps/vgmstream/layout/../base/../util/log.h \
+ deps/vgmstream/layout/../base/play_state.h \
+ deps/vgmstream/layout/../base/render.h \
+ deps/vgmstream/layout/../base/rc.h
+deps/vgmstream/layout/layout.h:
+deps/vgmstream/layout/../streamtypes.h:
+deps/vgmstream/layout/../vgmstream.h:
+deps/vgmstream/layout/../util/vgmstream_limits.h:
+deps/vgmstream/layout/../streamfile.h:
+deps/vgmstream/layout/../streamtypes.h:
+deps/vgmstream/layout/../util.h:
+deps/vgmstream/layout/../util/reader_get.h:
+deps/vgmstream/layout/../util/reader_put.h:
+deps/vgmstream/layout/../vgmstream_types.h:
+deps/vgmstream/layout/../coding/libs/g72x_vgmstream.h:
+deps/vgmstream/layout/../util/reader_sf.h:
+deps/vgmstream/layout/../util/../streamfile.h:
+deps/vgmstream/layout/../util/reader_get.h:
+deps/vgmstream/layout/../util/log.h:
+deps/vgmstream/layout/../base/sbuf.h:
+deps/vgmstream/layout/../base/../streamtypes.h:
+deps/vgmstream/layout/../base/rc.h:
+deps/vgmstream/layout/../base/seek.h:
+deps/vgmstream/layout/../base/../vgmstream.h:
+deps/vgmstream/layout/../base/decode.h:
+deps/vgmstream/layout/../base/sbuf.h:
+deps/vgmstream/layout/../base/mixing.h:
+deps/vgmstream/layout/../base/../util/log.h:
+deps/vgmstream/layout/../base/play_state.h:
+deps/vgmstream/layout/../base/render.h:
+deps/vgmstream/layout/../base/rc.h:
