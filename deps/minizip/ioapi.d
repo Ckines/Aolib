a9d07172900cfe007dfc9d78682f34d9440a43d2
J. Castro <jvgc1991@gmail.com>
2026-08-24 17:44:09 +0200

    Add files via upload

---
+++ deps/minizip/ioapi.d
@@ -0,0 +1,5 @@
+deps/minizip/ioapi.o: deps/minizip/ioapi.c deps/minizip/ioapi.h \
+ deps/aosdk/zlib/zlib.h deps/aosdk/zlib/zconf.h
+deps/minizip/ioapi.h:
+deps/aosdk/zlib/zlib.h:
+deps/aosdk/zlib/zconf.h:
---
+++ deps/minizip/unzip.d
@@ -0,0 +1,6 @@
+deps/minizip/unzip.o: deps/minizip/unzip.c deps/aosdk/zlib/zlib.h \
+ deps/aosdk/zlib/zconf.h deps/minizip/unzip.h deps/minizip/ioapi.h
+deps/aosdk/zlib/zlib.h:
+deps/aosdk/zlib/zconf.h:
+deps/minizip/unzip.h:
+deps/minizip/ioapi.h:
