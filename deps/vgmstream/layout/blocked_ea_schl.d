1ffb9a3ea392483d0a44843495cccd92ce491cd2
J. Castro <jvgc1991@gmail.com>
2026-08-24 17:03:09 +0200

    Add files via upload

---
+++ deps/vgmstream/layout/blocked_ea_schl.d
@@ -0,0 +1,53 @@
+deps/vgmstream/layout/blocked_ea_schl.o: \
+ deps/vgmstream/layout/blocked_ea_schl.c deps/vgmstream/layout/layout.h \
+ deps/vgmstream/layout/../streamtypes.h \
+ deps/vgmstream/layout/../vgmstream.h \
+ deps/vgmstream/layout/../util/vgmstream_limits.h \
+ deps/vgmstream/layout/../streamfile.h \
+ deps/vgmstream/layout/../streamtypes.h deps/vgmstream/layout/../util.h \
+ deps/vgmstream/layout/../util/reader_get.h \
+ deps/vgmstream/layout/../util/reader_put.h \
+ deps/vgmstream/layout/../vgmstream_types.h \
+ deps/vgmstream/layout/../coding/libs/g72x_vgmstream.h \
+ deps/vgmstream/layout/../util/reader_sf.h \
+ deps/vgmstream/layout/../util/../streamfile.h \
+ deps/vgmstream/layout/../util/reader_get.h \
+ deps/vgmstream/layout/../util/log.h deps/vgmstream/layout/../base/sbuf.h \
+ deps/vgmstream/layout/../base/../streamtypes.h \
+ deps/vgmstream/layout/../base/rc.h \
+ deps/vgmstream/layout/../coding/coding.h \
+ deps/vgmstream/layout/../coding/../vgmstream.h \
+ deps/vgmstream/layout/../coding/../util/reader_sf.h \
+ deps/vgmstream/layout/../coding/../util/reader_get_nibbles.h \
+ deps/vgmstream/layout/../coding/../util/../streamtypes.h \
+ deps/vgmstream/layout/../coding/../util/log.h \
+ deps/vgmstream/layout/../coding/libs/clhca.h \
+ deps/vgmstream/layout/../util/endianness.h \
+ deps/vgmstream/layout/../util/reader_sf.h
+deps/vgmstream/layout/layout.h:
+deps/vgmstream/layout/../streamtypes.h:
+deps/vgmstream/layout/../vgmstream.h:
+deps/vgmstream/layout/../util/vgmstream_limits.h:
+deps/vgmstream/layout/../streamfile.h:
+deps/vgmstream/layout/../streamtypes.h:
+deps/vgmstream/layout/../util.h:
+deps/vgmstream/layout/../util/reader_get.h:
+deps/vgmstream/layout/../util/reader_put.h:
+deps/vgmstream/layout/../vgmstream_types.h:
+deps/vgmstream/layout/../coding/libs/g72x_vgmstream.h:
+deps/vgmstream/layout/../util/reader_sf.h:
+deps/vgmstream/layout/../util/../streamfile.h:
+deps/vgmstream/layout/../util/reader_get.h:
+deps/vgmstream/layout/../util/log.h:
+deps/vgmstream/layout/../base/sbuf.h:
+deps/vgmstream/layout/../base/../streamtypes.h:
+deps/vgmstream/layout/../base/rc.h:
+deps/vgmstream/layout/../coding/coding.h:
+deps/vgmstream/layout/../coding/../vgmstream.h:
+deps/vgmstream/layout/../coding/../util/reader_sf.h:
+deps/vgmstream/layout/../coding/../util/reader_get_nibbles.h:
+deps/vgmstream/layout/../coding/../util/../streamtypes.h:
+deps/vgmstream/layout/../coding/../util/log.h:
+deps/vgmstream/layout/../coding/libs/clhca.h:
+deps/vgmstream/layout/../util/endianness.h:
+deps/vgmstream/layout/../util/reader_sf.h:
